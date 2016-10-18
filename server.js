83146b88ada71df768a9ee8a967882232983b359
Christoph Herr <info@christophherr.com>
2016-10-18 02:27:29 +0000

    Adds functionality

--- server.js
+++ server.js
@@ -1,84 +1,31 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var useragent = require('useragent');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+var app = express();
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+app.set('port', process.env.PORT);
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
+app.get('/', function(req, res) {
+  
+  var visitor = useragent.parse(req.headers['user-agent']);
+ 
+  var visitorIP = req.headers["x-forwarded-for"];
+  
+  if (visitorIP) {
+    var IPlist = visitorIP.split(",");
+    visitorIP = IPlist[IPlist.length -1];
+  } else {
+    visitorIP = req.connection.remoteAddress;
+  }
+  
+  res.json({
+    "IP-Address": visitorIP,
+    "Language": req.headers['accept-language'].split(',')[0],
+    "Operating System": visitor.os.family
   });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node.js Server is listening on port ' + app.get('port'));
+  });
